6c87ad085c2085d04fa5b41b4eef502d90586afd
ALEX ENRIQUEZ <alexis.enriquezca@gmail.com>
2021-12-28 18:11:48 -0500

    refactor: Refactoring project structure

---
+++ blog-backend/src/Core/Application/Interfaces/IPostService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IPostService
+    {
+        Task<IAsyncEnumerable<string>> GetAllAsync();
+        Task<string> Save(string id);
+    }
+}
---
+++ blog-backend/src/Presentation/Api/Controllers/PostsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        public PostsController()
+        {
+
+        }
+    }
+}
